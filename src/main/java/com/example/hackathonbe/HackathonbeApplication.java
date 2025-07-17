b3ac32514356bd7942911b851d4074800ad3072f
MingTori <s090ldbread@gmail.com>
2025-07-17 14:43:40 +0900

    :gear: CD 구축

---
+++ src/main/java/com/example/hackathonbe/HackathonbeApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackathonbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonbeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackathonbeApplication.class, args);
+    }
+
+}
